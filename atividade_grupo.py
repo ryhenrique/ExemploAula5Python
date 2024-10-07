8c4020791ce0cdccf497689bd5106c29ba3a3160
Leandro-tec-ti <leandro.tec.ti@hotmail.com>
2024-10-07 19:01:10 -0300

    atividade grupo
    Leandro e Ryan

---
+++ atividade_grupo.py
@@ -0,0 +1,14 @@
+# Faça um programa que se o usuário inserir 
+#1 = ele escolhe a classe Bárbara
+#2 = ele escolhe a classe a clase mago 
+#3 = ele escolhe a classe arqueiro
+
+numero = int(input(" \n 1 = ele escolhe a classe Bárbara \n 2 = ele escolhe a classe a clase mago \n 3 = ele escolhe a classe arqueiro \n Qual classe voçe deseja? "))
+
+
+if(numero == 1):
+    print("sua classe é barbara ")
+elif(numero == 2):
+    print("sua classe é mago ")
+else:
+    print("sua classe é arqueiro")
